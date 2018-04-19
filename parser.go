e568fb0eaea7fe98485465c3df2352f0af503923
Borys Hulii <8tomat8@gmail.com>
2018-04-19 19:36:30 +0200

    Added config parser implementation

---
+++ parser.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"github.com/jessevdk/go-flags"
+	"github.com/pkg/errors"
+)
+
+// configurable is interface implemented by BaseConfig
+type configurable interface {
+	SetConfigFunc(func(string) error)
+}
+
+// BaseConfig should be embed into your configuration struct
+type BaseConfig struct {
+	Config func(s string) error `long:"config" description:"The ini config file" no-ini:"true"`
+	Global struct {
+		DialogVersion string `long:"global-dialog-version" required:"true" ini-name:"dialog-version"`
+	} `group:"global"`
+}
+
+func (b *BaseConfig) SetConfigFunc(f func(s string) error) {
+	b.Config = f
+}
+
+// ParseConfig parses configuration file provided in --config flag
+// cfgStruct should be a pointer
+func Parse(cfgStruct interface{}) error {
+	cfg, ok := cfgStruct.(configurable)
+	if !ok {
+		return errors.New("struct BaseConfig must be embed into cfgStruct")
+	}
+	p := flags.NewParser(cfg, flags.None)
+
+	cfg.SetConfigFunc(func(s string) error {
+		ini := flags.NewIniParser(p)
+
+		return ini.ParseFile(s)
+	})
+
+	_, err := p.Parse()
+	return errors.Wrap(err, "failed to parse config file")
+}
